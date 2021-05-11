df0587a16a1a8832822916674fd106ae12a7acc6
Shimrit Herbst <shimrit.levine@gmail.com>
2021-05-11 11:40:59 +0300

    remove comment

--- frontend/src/services/httpService.js
+++ frontend/src/services/httpService.js
@@ -3,8 +3,6 @@ import Axios from 'axios';
 const BASE_URL = process.env.NODE_ENV === 'production'
     ? '/api/'
     : '//localhost:3030/'
-// ? '/api/'
-// : '//localhost:3030/api/'
 
 var axios = Axios.create({
     withCredentials: true
