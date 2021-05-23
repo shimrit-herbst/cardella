915029dbfcf52c9225ba4622f236cb8319242c9a
Shimrit Herbst <shimrit.levine@gmail.com>
2021-05-23 11:47:22 +0300

    Board-memebrs cmp and fixes for app-header btns

--- frontend/src/cmps/Board/BoardHeader/BoardHeader.jsx
+++ frontend/src/cmps/Board/BoardHeader/BoardHeader.jsx
@@ -1,3 +1,4 @@
+import Members from '../../Members/Members';
 import './BoardHeader.scss';
 
 function BoardHeader({ board }) {
@@ -5,6 +6,9 @@ function BoardHeader({ board }) {
         <div className="board-header grid">
             <div className="left-board-header flex">
                 <h2 className="board-title fs18">{board.title}</h2>
+            </div>
+            <div className="avatar-board-header flex">
+                {board.members && <Members members={board.members} />}
             </div>
         </div>
     )
---
+++ frontend/src/cmps/Members/index.js
@@ -0,0 +1,3 @@
+import Members from '../Members.jsx';
+
+export default Members;
