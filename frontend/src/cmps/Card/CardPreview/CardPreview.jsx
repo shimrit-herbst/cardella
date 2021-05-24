2f466dc7fd02ec678c48c8f59760ca19df7a9afb
Shimrit Herbst <shimrit.levine@gmail.com>
2021-05-24 10:18:09 +0300

    Re-name style property (camalCase instead of kebab-case)

--- frontend/src/cmps/Card/CardPreview/CardPreview.jsx
+++ frontend/src/cmps/Card/CardPreview/CardPreview.jsx
@@ -2,7 +2,7 @@ import './CardPreview.scss';
 
 function CardPreview({ card }) {
     return (
-        <div className="card-preview-container" style={{ 'background-color': card.style.bgColor }}>
+        <div className="card-preview-container" style={{ backgroundColor: card.style.bgColor }}>
             {card.uploadImgUrl && <img src={card.uploadImgUrl} alt="" />}
             <div className="card-content fs-14">
                 <h4>{card.title}</h4>
