e5f723db583c311af12e9a035d3fc3a46b44be88
Shimrit Herbst <shimrit.levine@gmail.com>
2021-05-25 12:00:52 +0300

    feature/list-menu-cmp

--- frontend/src/cmps/AppHeader/AppHeader.jsx
+++ frontend/src/cmps/AppHeader/AppHeader.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome, faTh } from '@fortawesome/fontawesome-free-solid';
